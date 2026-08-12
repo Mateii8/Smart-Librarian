92fd472f06db415b50f9153b747ad7888d7c0e4d
Ion Matei <ionmatei8585@gmail.com>
2026-08-12 11:23:24 +0300

    Modify app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,6 @@ function App() {
 
   const messagesEndRef = useRef(null);
 
-  // Save chats
 
   useEffect(() => {
 
@@ -58,7 +57,6 @@ function App() {
 
   }, [chats]);
 
-  // Auto scroll
 
   useEffect(() => {
 
