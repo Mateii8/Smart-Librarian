394a05ef4c3799ea1317855b448deb6fc5202873
Ion Matei <ionmatei8585@gmail.com>
2026-08-19 15:25:54 +0300

    Allow GitHub Pages CORS

--- backend/api.py
+++ backend/api.py
@@ -23,7 +23,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
         "https://mateii8.github.io",
     ],
     allow_credentials=True,
