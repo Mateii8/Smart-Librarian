1a319a81ee5e0810613fad164f70b14e4d1f9d99
Ion Matei <ionmatei8585@gmail.com>
2026-08-19 15:23:57 +0300

    Allow GitHub Pages CORS

--- backend/api.py
+++ backend/api.py
@@ -21,7 +21,11 @@ openai_client = OpenAI(api_key=OPENAI_API_KEY)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://mateii8.github.io",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
